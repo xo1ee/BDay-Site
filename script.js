edefd4b96d6f1f0c8ec6ce494dd6d7c239eb4a0c
xo1ee <126421239+xo1ee@users.noreply.github.com>
2024-06-19 23:37:23 -0700

    changes how alt font color is done, did alt letter tilt, added cloud for testing, added images to choose

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-$("span").each(function (index) {
-  var originalText = $(this).text();
+$(document).ready(function() {
+  var titleText = $("#title").text(); // Get the text content of the title
   var newText = "";
-  for (var i = 0; i < originalText.length; i++) {
-    if (i % 2 === 0) newText += "<span>" + originalText.charAt(i) + "</span>";
-    else newText += originalText.charAt(i);
+
+  // Loop through each character in the title text
+  for (var i = 0; i < titleText.length; i++) {
+      var char = titleText.charAt(i);
+
+      // Check if the character is a space
+      if (char === " ") {
+          newText += " "; // Preserve space with a simple space character
+      } else {
+          var spanClass = i % 2 === 0 ? "color1" : "color2"; // Alternate color classes
+
+          // Create a span for each character with the appropriate color class and tilt class
+          newText += "<span class='" + spanClass + " tilted'>" + char + "</span>";
+      }
   }
-  $(this).html(newText);
+
+  // Replace the content of the title with the newly constructed HTML
+  $("#title").html(newText);
 });
